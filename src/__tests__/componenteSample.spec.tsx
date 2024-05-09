864fc375e703f5f3e28056bf6b1777e32b675732
AyakaKoyama <ayamail2000@gmail.com>
2024-05-10 08:25:56 +0900

    Initial commit

---
+++ src/__tests__/componenteSample.spec.tsx
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("Title Test", () => {
+  it("タイトルがHello Jestであること", () => {
+    render(<App />);
+    const title = screen.getByTestId("title");
+    expect(title).toHaveTextContent("Hello Jest");
+  });
+});
