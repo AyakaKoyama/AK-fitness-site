bf8c035db79601a31f63e3a0c4b6aae8033d2fb5
AyakaKoyama <ayamail2000@gmail.com>
2024-05-11 11:50:30 +0900

    supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@chakra-ui/react";
 import React from "react";
 
 function App() {
   return (
     <>
       <title data-testid="title">Hello Jest</title>
+      <Button>Chakura UI</Button>
     </>
   );
 }
